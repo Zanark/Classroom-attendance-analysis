4053427cd5a4932f5efc4d6a7a662d6ea0be7815
Zanark <kiodeba11@gmail.com>
2019-09-18 20:34:04 +0530

    testing facial recognition on omm and my face data

--- facerec_from_webcam_faster.py
+++ facerec_from_webcam_faster.py
@@ -2,34 +2,25 @@ import face_recognition
 import cv2
 import numpy as np
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("obama.jpg")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+zanark_image = face_recognition.load_image_file("zanark.jpg")
+zanark_face_encoding = face_recognition.face_encodings(zanark_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("biden.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+omm_image = face_recognition.load_image_file("omm.jpg")
+omm_face_encoding = face_recognition.face_encodings(omm_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
-    obama_face_encoding,
-    biden_face_encoding
+    omm_face_encoding,
+    zanark_face_encoding
 ]
 known_face_names = [
-    "Barack Obama",
-    "Joe Biden"
+    "Zanark",
+    "Omm"
 ]
 
 # Initialize some variables
